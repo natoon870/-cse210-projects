6f259eb09bea57801cf721b6cb822116bab1acc5
natoon870 <tay22034@byui.edu>
2025-09-18 15:05:27 -0600

    built a program that asks for the users grade percentage and converts to a letter grade and prompts them if they passed the class or not.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        int gradePercentage = int.Parse(grade);
+        string letter = "";
+
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is a/an {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine($"Since your grade was a/an {letter}, you have passed the course!");
+
+        }
+        else
+        {
+            Console.WriteLine($"Sorry!! Since your grade was a/an {letter}, you have not passed the course. Better luck next time!");
+        }
     }
 }
