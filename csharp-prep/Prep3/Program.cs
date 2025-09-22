800566b8a53e6eb10ec0663b42200805296b3486
natoon870 <tay22034@byui.edu>
2025-09-22 11:52:58 -0600

    created a guessing game that asks the user to guess a random generated number from 0-100 and prompts them if their guesses are higher or lower. The program will lopp until the user guesses the random generated number.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        Console.WriteLine("Welcome to the magic number guessing game where you will be guessing a number from 0-100! \n");
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < number)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
