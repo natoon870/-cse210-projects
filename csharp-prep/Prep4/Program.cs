2f28fb74266d1552a2b86b593dcaea457f2ade57
natoon870 <tay22034@byui.edu>
2025-09-23 14:22:17 -0600

    Created a program that takes the value from user input and stores it in a list. The user is able to store as many values as they want. When they are done, this program returns the sum, average, and max of the list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.\n ");
+        int entry = -1;
+
+        while (entry != 0)
+        {
+            Console.Write("Enter number:  ");
+            entry = int.Parse(Console.ReadLine());
+
+            if (entry != 0)
+            {
+                numbers.Add(entry);
+            }
+        }
+
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+        Console.WriteLine($"The sum is: {total}");
+
+        float length = numbers.Count;
+        float average = (float)total / length;
+        Console.WriteLine($"The average is: {average}");
+
+        int largestNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        int entry = -1;
+        while (entry != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            entry = int.Parse(Console.ReadLine());
+
+            if (entry != 1)
+            {
+                numbers.Add(entry);
+            }
+        }
+
+        
+        Console.WriteLine(numbers.Count);
+
+        
     }
 }
