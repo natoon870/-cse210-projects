590f7572b400a67d24078f439c90f7e19da23697
natoon870 <tay22034@byui.edu>
2025-09-25 11:49:12 -0600

    created a program that uses functions to find a users name, favorite number, and birth year. The program displays the name, favorite number squared and the age the user will be turning this year.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,28 +1,54 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
+        DisplayWelcome();
+        string username = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int birthYear = PromtUserBirthYear();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(username, squareNumber, birthYear);
 
-        List<int> numbers = new List<int>();
-        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
-        int entry = -1;
-        while (entry != 0)
+
+
+        static void DisplayWelcome()
         {
-            Console.WriteLine("Enter number: ");
-            entry = int.Parse(Console.ReadLine());
-
-            if (entry != 1)
-            {
-                numbers.Add(entry);
-            }
+            Console.WriteLine("Welcome to the program!");
         }
 
-        
-        Console.WriteLine(numbers.Count);
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+        static int PromtUserBirthYear()
+        {
+            Console.Write("Please enter the year you were born: ");
+            int year = int.Parse(Console.ReadLine());
+            return year;
+        }
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+        static void DisplayResult(string name, int square, int year)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+            Console.WriteLine($"{name}, you will turn {2025 - year} this year.");
+        }
 
+    } 
         
-    }
 }
