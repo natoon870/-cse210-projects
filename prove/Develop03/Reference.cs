75feafa17ca3413b5005bb69a4d0da295bd8a036
natoon870 <tay22034@byui.edu>
2025-10-26 10:44:25 -0600

    camal cased attributes

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -2,26 +2,26 @@ public class Reference
 {
     private string _book;
     private int _chapter;
-    private int _startverse;
-    private int _endverse;
+    private int _startVerse;
+    private int _endVerse;
 
     public Reference(string book, int chapter, int startverse, int endverse)
     {
         _book = book;
         _chapter = chapter;
-        _startverse = startverse;
-        _endverse = endverse;
+        _startVerse = startverse;
+        _endVerse = endverse;
 
     }
     public string ReferenceDisplay()
     {
-        if (_startverse == _endverse)
+        if (_startVerse == _endVerse)
         {
-            return $"{_book} {_chapter}:{_startverse}";
+            return $"{_book} {_chapter}:{_startVerse}";
         }
         else
         {
-            return $"{_book} {_chapter}:{_startverse}-{_endverse}";
+            return $"{_book} {_chapter}:{_startVerse}-{_endVerse}";
         }
     }
 }
