8984369b1f39eedf8fe18d888bce9c6e385a9dff
natoon870 <tay22034@byui.edu>
2025-10-30 12:11:09 -0600

    created a program that represents different types of programming assingments.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+    
+        Assignment a1 = new Assignment("Superman", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        
+        MathAssignment a2 = new MathAssignment("Batman", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Flash", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
