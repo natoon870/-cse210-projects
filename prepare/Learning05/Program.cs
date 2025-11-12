b0e543a157bdb75ffbb6bc5ef2f33a337c5a6805
natoon870 <tay22034@byui.edu>
2025-11-12 12:07:44 -0700

    created a program that finds the area of a square, rectangle, and square.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Learning05
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning05 World!");
+        static void Main(string[] args)
+        {
+            List<Shape> Shapes = new List<Shape>();
+
+            Square s1 = new Square("Red", 3);
+            Shapes.Add(s1);
+
+            Rectangle s2 = new Rectangle("Blue", 4, 5);
+            Shapes.Add(s2);
+
+            Circle s3 = new Circle("Green", 6);
+            Shapes.Add(s3);
+
+            foreach (Shape s in Shapes)
+            {
+                string color = s.GetColor();
+                double area = s.GetArea();
+                Console.WriteLine($"The {color} shape has an area of {area}.");
+            }
+        }
     }
 }
